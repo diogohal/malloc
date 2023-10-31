9ae86a9f597060cc795dc3b8ce63d417e78efb43
Pedro Willian Aguiar <pwa21@inf.ufpr.br>
2023-10-31 15:07:47 -0300

    problems with return

--- malloc.s
+++ malloc.s
@@ -1,19 +1,25 @@
 .section .data
-    topoInicialHeap: .quad 5
+    topoInicialHeap: .quad 0
 .section .text
 .globl _start
 _start:
-
+    call alocaMem
+    # movq topoInicialHeap, %rbx
     call iniciaAlocador
     movq topoInicialHeap, %rbx
     movq $60, %rax
-    movq topoInicialHeap, %rdi
-    movq %rbx, %rdi
+    movq (%rbx), %rdi
     syscall
 
 iniciaAlocador:
     movq $12, %rax
+    movq $0, %rdi
     syscall 
     movq %rax, topoInicialHeap
     ret
 
+alocaMem:
+    movq $12, %rax
+    movq $100, %rdi
+    syscall
+    ret
